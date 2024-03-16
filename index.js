452d3935da21ed6ed164f37bfd9757f424ea794d
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-15 23:30:46 -0400

    basic math operations

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+
+    return num1 + num2;
+
+}
+
+function subtract(num1, num2) {
+
+    return num1 - num2;
+
+}
+
+function product(num1, num2) {
+
+    return num1 * num2;
+
+}
+
+function divide(num1, num2) {
+
+    return num1 / num2;
+}
+
+console.log((3, 6));
