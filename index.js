96c6c7f8295677d6bf61af530ab9fbe86b948d5a
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-15 23:37:33 -0400

    operate function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let firstNumber = 0;
+
+let operand = "";
+
+let secondNumber = 0;
+
+
 function add(num1, num2) {
 
     return num1 + num2;
@@ -21,4 +28,16 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log((3, 6));
+function operate(operator, num1, num2) {
+
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "*") {
+        return product(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    }
+    
+}
